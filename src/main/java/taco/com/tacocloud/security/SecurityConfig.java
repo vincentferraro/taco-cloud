498bfc7df3ea1510b146532cd7c5bc482830b134
vincentferraro <v.ferraro@live.fr>
2024-01-24 12:05:45 -0500

    security and register and login view

--- src/main/java/taco/com/tacocloud/security/SecurityConfig.java
+++ src/main/java/taco/com/tacocloud/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package taco.com.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import taco.com.tacocloud.data.UserRepository;
 
@@ -32,6 +34,15 @@ public class SecurityConfig {
             throw new UsernameNotFoundException("User'"+username+"' not found");
         };
     }
+
+    @Bean  
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+        .antMatchers("/design", "/orders").access("hasRole('USER')")
+        .antMatchers("/", "/**").access("permitAll()")
+        .and().formLogin().loginPage("/login") // Call login page "login.html"
+        .and().build(); }
+
 
     
 }
